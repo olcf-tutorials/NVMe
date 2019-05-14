36ecbf7283c2fce29e26d9181d7702f7ff4fe440
Georgios Markomanolis <george@markomanolis.com>
2019-05-14 16:00:11 -0400

    create metadata folder

---
+++ metadata/nvme_metadata.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#BSUB -P #Account
+#BSUB -J NVME_Metadata
+#BSUB -o nvme_medatata.o%J
+#BSUB -W 2
+#BSUB -nnodes 1
+#BSUB -alloc_flags NVME
+
+
+export BBPATH=/mnt/bb/$USER
+
+jsrun -n 1 cp metadata ${BBPATH}
+jsrun -n 2 -r 2 -a 1 -c 1 --chdir ${BBPATH} ./metadata
